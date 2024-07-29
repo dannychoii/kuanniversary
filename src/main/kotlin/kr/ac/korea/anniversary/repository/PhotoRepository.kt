2daf7319db1e88b0371ba5481a8390a9ed2db703
dwchoi <nojamperson@gmail.com>
2024-07-29 22:22:06 +0900

    <FEAT> add photo feature 2

--- src/main/kotlin/kr/ac/korea/anniversary/repository/PhotoRepository.kt
+++ src/main/kotlin/kr/ac/korea/anniversary/repository/PhotoRepository.kt
@@ -12,7 +12,7 @@ class PhotoRepository(
 ) {
     fun findById(id: Long): Photo? {
         //language=sql
-        val sql = "SELECT id, imageUrl, created_at \nFROM photo \nWHERE id = ?"
+        val sql = "SELECT id, image_url, created_at \nFROM photo \nWHERE id = ?"
         return DataAccessUtils.singleResult(
             jdbcTemplate.query(sql, { rs, _ ->
                 Photo(
@@ -27,7 +27,7 @@ class PhotoRepository(
     fun search(pageCommand: PageCommand): Pair<List<Photo>, Long> {
         //language=sql
         val sql =
-            "SELECT id, imageUrl, created_at \nFROM photo \nWHERE id = ?\nLIMIT ${pageCommand.pageSize} \nOFFSET ${pageCommand.page * pageCommand.pageSize} \n"
+            "SELECT id, image_url, created_at \nFROM photo ORDER BY  id DESC \n LIMIT ${pageCommand.pageSize} \nOFFSET ${pageCommand.page * pageCommand.pageSize} \n"
         //language=sql
         val countSql = "SELECT count(*) \nFROM photo"
         val totalCount = jdbcTemplate.query(countSql) { rs, _ -> rs.getLong(1) }.first()
